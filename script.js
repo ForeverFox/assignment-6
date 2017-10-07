af28c562c36ca2bd114e7d488f590e87f0e2d0b0
Forever Fox <cogden@mcad.edu>
2017-10-07 20:07:35 +0300

    Some JS stuff

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var churchillSpeech = {
       'authorAge': '66'
     },
     ghandiSpeech = {
-      'author': 'Ghandi',
+      'author': 'Ghandi', // It's supposed to be "Gandhi."
       'year': 1942,
       'yearIsBCE': false,
       'authorAge': '73'
@@ -21,83 +21,75 @@ var churchillSpeech = {
     userNamePrompt,
     favoriteSpeechPrompt;
 
+var i = 0;
+var oldestYear = speechesArray[0].year;
+var newestYear = speechesArray[0].year;
+
+while(i < speechesArray.length){
+  console.log("This speech is written by " + speechesArray[i].author);
+  i++;
+};        
+
+
+for (i = 0; i < speechesArray.length; i++) {
+  if (speechesArray[i].year < oldestYear) {
+    oldestYear = speechesArray[i].year;
+  } else if (speechesArray[i].year > newestYear) {
+    newestYear = speechesArray[i].year;
+  }; 
+};
+
+/* In this case, regarding the window prompt functionality, there is no requirement to use the console.log within the while loop. I think you should maybe reconsider whether you want your console.log functions executing from within the loop. Note that code that follows the loop will still execute once the loop is finished - the `break` statement should only break us out of the while loop and not the entire click event. Consider setting some default variables before the loop runs, and then when you run your loop and your condition is met within that loop, set those variables again. After the loop has ran, try running another conditional check and execute either the correct log statement or the error warning statement. 
+
+Try for loop outside of function / add variables / ??
+
+*/
+
 document.getElementById('BtnDonate').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Donate" button.
   favoriteSpeechPrompt = window.prompt('Which speech author is your favorite?');
-
-  switch(favoriteSpeechPrompt){
-    case 'Churchill':
-      console.log(speechesArray[0].author + ' was ' + speechesArray[0].authorAge + ' during this speech.');
-      break;
-    case 'Ghandi':
-      console.log(speechesArray[1].author + ' was ' + speechesArray[1].authorAge + ' during this speech.');
-      break;
-    case 'Demosthenes':
-      console.log(speechesArray[2].author + ' was ' + speechesArray[2].authorAge + ' during this speech.');
-      break;
-    default:
-      console.log('Did you spell that name correctly?');
-      break;
-  }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+  for (i = 0; i < speechesArray.length; i++) {
+      if (favoriteSpeechPrompt === speechesArray[i].author) { 
+          console.log(speechesArray[i].author + " was " + speechesArray[i].authorAge + " during this speech.");
+      }
+  };
 });
+
+
+/* if (favoriteSpeechPrompt !== speechesArray.author) {
+  console.log("Did you spell the author's name correctly?"); 
+}; */
+
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Churchill" button.
-  console.log('This speech was written by ' + speechesArray[0].author + ' in ' + speechesArray[0].year);
-
-  if(speechesArray[0].yearIsBCE === true){
-    console.log('This speech took place before the common era.');
-  }else{
-    console.log('This speech took place during the common era.');
-  }
-
-  if(speechesArray[0].year < speechesArray[1].year && speechesArray[0].year < speechesArray[2].year){
-    console.log('This is the oldest speech on the page.');
-  }else if(speechesArray[0].year > speechesArray[1].year && speechesArray[0].year > speechesArray[2].year){
-    console.log('This is the most recent speech on the page.');
-  }
-});
+   console.log('This speech was written by ' + speechesArray[0].author + ' in ' + speechesArray[0].year);
+   if (speechesArray[0].year <= oldestYear) {
+      console.log("This is the oldest speech on the page.");
+   } else if (speechesArray[0].year >= newestYear) {
+      console.log("This is the most recent speech on the page.");
+   };
+}); 
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Ghandi" button.
   console.log('This speech was written by ' + speechesArray[1].author + ' in ' + speechesArray[1].year);
 
-  if(speechesArray[1].yearIsBCE === true){
-    console.log('This speech took place before the common era.');
-  }else{
-    console.log('This speech took place during the common era.');
-  }
-
-  if(speechesArray[1].year < speechesArray[0].year && speechesArray[1].year < speechesArray[2].year){
-    console.log('This is the oldest speech on the page.');
-  }else if(speechesArray[1].year > speechesArray[0].year && speechesArray[1].year > speechesArray[2].year){
-    console.log('This is the most recent speech on the page.');
-  }
+   if (speechesArray[1].year <= oldestYear) {
+      console.log("This is the oldest speech on the page.");
+   } else if (speechesArray[1].year >= newestYear) {
+      console.log("This is the most recent speech on the page.");
+   };
 });
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
   //Code in here executes when the user clicks the "Demosthenes" button.
   console.log('This speech was written by ' + speechesArray[2].author + ' in ' + speechesArray[2].year);
 
-  if(speechesArray[2].yearIsBCE === true){
-    console.log('This speech took place before the common era.');
-  }else{
-    console.log('This speech took place during the common era.');
-  }
-
-  if(speechesArray[2].year < speechesArray[0].year && speechesArray[2].year < speechesArray[1].year){
-    console.log('This is the oldest speech on the page.');
-  }else if(speechesArray[2].year > speechesArray[0].year && speechesArray[2].year > speechesArray[1].year){
-    console.log('This is the most recent speech on the page.');
-  }
+   if (speechesArray[2].year <= oldestYear) {
+      console.log("This is the oldest speech on the page.");
+   } else if (speechesArray[2].year >= newestYear) {
+      console.log("This is the most recent speech on the page.");
+   };
 });
-
-
-userNamePrompt = window.prompt('Hello, what is your name?');
-
-if(typeof userNamePrompt === 'string' && userNamePrompt !== ''){
-  console.log('Hi ' + userNamePrompt + '!');
-}else{
-  console.log('Ok, I\'ll just call you User.');
-  userNamePrompt = 'User';
-}
